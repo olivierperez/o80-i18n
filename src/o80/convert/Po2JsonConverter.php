31bfd2910a3c60bd9b6087d2c38981eb69cbbf7d
Olivier PEREZ <olivier@olivierperez.fr>
2015-03-26 20:15:40 +0100

    Fix error when using JSON_PRETTY_PRINT before PHP 5.4

--- src/o80/convert/Po2JsonConverter.php
+++ src/o80/convert/Po2JsonConverter.php
@@ -32,9 +32,55 @@ class Po2JsonConverter extends FromPoConverter {
     }
 
     protected function toString() {
-        $json = json_encode($this->json, JSON_PRETTY_PRINT | ~(JSON_ERROR_UTF8 | JSON_HEX_QUOT | JSON_HEX_APOS));
+        $json = PHP_VERSION_ID >= 50400 ?
+            json_encode($this->json, JSON_PRETTY_PRINT | ~(JSON_ERROR_UTF8 | JSON_HEX_QUOT | JSON_HEX_APOS)) :
+            $this->prettyPrint(json_encode($this->json, ~(JSON_ERROR_UTF8 | JSON_HEX_QUOT | JSON_HEX_APOS)));
         $json = str_replace('\\\\n', '\\n', $json);
         $json = str_replace('\\\\\\"', '\\"', $json);
         return $json;
+    }
+
+    /**
+     * Picked up on https://gist.github.com/odan/7a04c02dbce59217a33c gist.
+     *
+     * @param string $json JSON object
+     * @return string The String representation of JSON ojbect
+     */
+    private function prettyPrint($json) {
+        $tokens = preg_split('|([\{\}\]\[,])|', $json, -1, PREG_SPLIT_DELIM_CAPTURE);
+        $result = '';
+        $indent = 0;
+        $ind = '  ';
+        $lineBreak = "\n";
+
+        $inLiteral = false;
+        foreach ($tokens as $token) {
+            if ($token == '') {
+                continue;
+            }
+            $prefix = str_repeat($ind, $indent);
+            if (!$inLiteral && ($token == '{' || $token == '[')) {
+                $indent++;
+                if (($result != '') && ($result[(strlen($result) - 1)] == $lineBreak)) {
+                    $result .= $prefix;
+                }
+                $result .= $token . $lineBreak;
+            } elseif (!$inLiteral && ($token == '}' || $token == ']')) {
+                $indent--;
+                $prefix = str_repeat($ind, $indent);
+                $result .= $lineBreak . $prefix . $token;
+            } elseif (!$inLiteral && $token == ',') {
+                $result .= $token . $lineBreak;
+            } else {
+                $result .= ($inLiteral ? '' : $prefix) . $token;
+                // Count # of unescaped double-quotes in token, subtract # of
+                // escaped double-quotes and if the result is odd then we are
+                // inside a string literal
+                if ((substr_count($token, "\"") - substr_count($token, "\\\"")) % 2 != 0) {
+                    $inLiteral = !$inLiteral;
+                }
+            }
+        }
+        return $result;
     }
 }
